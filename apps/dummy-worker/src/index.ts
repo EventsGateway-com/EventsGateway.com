d0c409fa73ffdb1288ee9ed1b218a80032474be0
agilexmarketing <marian@agilemedia.com>
2026-05-30 15:04:57 +0300

    Add dummy worker for sources domain

---
+++ apps/dummy-worker/src/index.ts
@@ -0,0 +1,11 @@
+export default {
+  async fetch() {
+    return new Response("dummy", {
+      status: 200,
+      headers: {
+        "content-type": "text/plain; charset=utf-8",
+        "cache-control": "no-store"
+      }
+    });
+  }
+};
